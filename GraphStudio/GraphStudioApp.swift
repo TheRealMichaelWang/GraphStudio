3a7badcd4fd07a321c743254a1374ee69db7376f
Michael Wang <mwang764@gatech.edu>
2025-12-21 21:13:24 -0800

    Initial Commit

---
+++ GraphStudio/GraphStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphStudioApp.swift
+//  GraphStudio
+//
+//  Created by Michael Wang on 12/21/25.
+//
+
+import SwiftUI
+
+@main
+struct GraphStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
